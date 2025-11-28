f8884c9ceaa1a6665d54c275aac3f95c56ab0fc5
ilegra-dbaste_fgc <ilegra.dbaste@fgc.org.br>
2025-11-28 10:04:29 -0300

    add arquivos referente ao teste idp

---
+++ templates/python-starter/content/main.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    print("Iniciando o serviço ${{ values.name }}...")
+    # Lógica do app aqui
+    print("Serviço rodando com sucesso!")
+
+if __name__ == "__main__":
+    main()
